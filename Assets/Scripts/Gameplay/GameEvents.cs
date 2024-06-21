8504c73cd1ed251b5c3143b10e925846af16d2a2
Sol Elan <solipod8@gmail.com>
2024-06-21 20:16:42 +0300

    Add idle gains to info panel on main panel

--- Assets/Scripts/Gameplay/GameEvents.cs
+++ Assets/Scripts/Gameplay/GameEvents.cs
@@ -3,9 +3,15 @@ using System;
 public static class GameEvents
 {
     public static event Action OnMapDataChanged;
+    public static event Action OnIdleGainsChanged;
 
     public static void MapDataChanged()
     {
         OnMapDataChanged?.Invoke();
+    }
+
+    public static void IdleGainsChanged()
+    {
+        OnIdleGainsChanged?.Invoke();
     }
 }
---
+++ Assets/Scripts/UI/Main/IdleGains.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+
+public class IdleGains : MonoBehaviour
+{
+    public TMP_Text goldText;
+    public TMP_Text essenceText;
+
+    void Start()
+    {
+        GameEvents.OnIdleGainsChanged += UpdateIdleGains;
+        UpdateIdleGains();
+    }
+
+    void OnDestroy()
+    {
+        GameEvents.OnMapDataChanged -= UpdateIdleGains;
+    }
+
+    public void UpdateIdleGains()
+    {
+        goldText.text = $"{GoldMine.GetTotalGoldFromAllMaps() / GoldMine.incomeLoopSeconds}/s";
+        essenceText.text = $"{EssenceLab.GetTotalEssenceFromAllMaps() / EssenceLab.incomeLoopSeconds}/s";
+    }
+}
