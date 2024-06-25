8bb86df1ed23526feb2daa59b52b03dd222db2c4
Sol Elan <solipod8@gmail.com>
2024-06-26 00:05:48 +0300

    Add dark background on maps panel open

--- Assets/Scripts/UI/DisplayPanel.cs
+++ Assets/Scripts/UI/DisplayPanel.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class DisplayPanel : MonoBehaviour
 {
     public GameObject panel;
+    public GameObject backgroundPanel;
 
     public void OpenPanel()
     {
+        
+        backgroundPanel?.SetActive(true);
         panel.SetActive(true);
     }
 
     public void ClosePanel()
     {
+        backgroundPanel?.SetActive(false);
         panel.SetActive(false);
     }
 }
